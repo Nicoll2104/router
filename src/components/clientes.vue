e86814f68fceef7885ab0f3efd692906612778cd
Nicoll2104 <soda210461@gmail.com>
2023-10-18 17:15:33 -0500

    actual

--- src/components/clientes.vue
+++ src/components/clientes.vue
@@ -1,192 +1,155 @@
 <template>
-  <div>
-    <q-btn label="Agregar" color="primary" @click="toolbar = true" />
-    <q-dialog v-model="toolbar">
-      <q-card>
-        <q-toolbar>
-        <q-toolbar-title>Encuesta</q-toolbar-title>
-        <q-btn flat round dense icon="❌" @click="toolbar = false" />
-        </q-toolbar>
-      <q-card-section>
-        <label for="">Cedula: </label><br />
-        <input type="number" v-model="cedula" />
-        <br />
-        <label for="">Nombre: </label><br />
-        <input type="text" v-model="nombre" />
-        <br />
-        <label for="">Apellido: </label><br />
-        <input type="text" v-model="apellido" /><br />
-        <label for="">Edad: </label><br />
-        <input type="number" v-model="edad" /><br />
-        <label for="">Telefono: </label><br />
-        <input type="number" v-model="telefono" /><br />
-        <label for="">Email: </label><br />
-        <input type="text" v-model="email" /><br />
-        <label for="">Contrasena: </label><br />
-        <input type="text" v-model="contrasena" /><br />
-        <label for="">Maleta:</label><br />
-        <input type="number" v-model="maleta" /><br />
-        <button @click="agregarCliente()">Enviar</button>
-      </q-card-section>
-    </q-card>
-  </q-dialog>
   <div class="q-pa-md">
-    <q-markup-table>
-        <thead>
-          <tr>
-            <th><h4 class="q-ma-xs text-left">Clientes</h4></th>
-          </tr>
-          <tr>
-            <th class="text-left"><b>Cedula</b></th>
-            <th class="text-right"><b>Nombre</b></th>
-            <th class="text-right"><b>Apellido</b></th>
-            <th class="text-right"><b>Edad</b></th>
-            <th class="text-right"><b>Telefono</b></th>
-            <th class="text-right"><b>Email</b></th>
-            <th class="text-right"><b>maleta</b></th>
-            <th class="text-right"><b>Status</b></th>
-            <th class="text-right"><b>Opciones</b></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="row in rows">
-            <td class="text-left">{{ row.cedula }}</td>
-            <td class="text-right">{{ row.nombre }}</td>
-            <td class="text-right">{{ row.apellido }}</td>
-            <td class="text-right">{{ row.edad }}</td>
-            <td class="text-right">{{ row.telefono }}</td>
-            <td class="text-right">{{ row.email }}</td>
-            <td class="text-right">{{ row.maleta }}</td>
-            <td class="text-right">{{ row.status }}</td>
-        </tr>
-      </tbody>
-    </q-markup-table>
+    <q-table title="DATOS TICKETS" :rows="DatosData" :columns="columns" row-key="cedula">
+      <template v-slot:body-cell-status="props">
+        <q-td key="status" :props="props">
+          <span class="color1" v-if="props.row.status == 1">Activo</span>
+          <span class="color2" v-else >Inactivo</span>
+        </q-td>
+      </template>
+      <template v-slot:body-cell-acciones="props">
+    <q-td key="acciones" :props="props">
+      <q-btn class="btnEditar" icon="edit" color="amber" @click="editarCliente(props.row)"></q-btn>
+      <q-btn class="btnActivar" v-if="props.row.status == 1" @click="desactivar(props.row._id)">❌</q-btn>
+      <q-btn class="btnActivar" v-else @click="activar(props.row._id)">✅</q-btn>
+    </q-td>
+  </template>
+    </q-table>
+    <q-dialog v-model="modal">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">DATOS DE CLIENTE</div>
+        </q-card-section>
+
+        <q-separator />
+
+        <q-card-section style="max-height: 50vh" class="scroll">
+          <div class="infoDatos">
+            <div class="ilDatos">
+              <label class="labelDatos" for="cedula" >Cedula:</label>
+              <input class="inputDatos" type="text" id="cedula" v-model="cedula"  />
+            </div>
+
+            <div class="ilDatos">
+              <label class="labelDatos" for="telefono" >Nombre:</label>
+              <input class="inputDatos" type="text" id="nombre" v-model="nombre" />
+            </div>
+
+            <div class="ilDatos">
+              <label class="labelDatos" for="telefono" >Telefono:</label>
+              <input class="inputDatos" type="text" id="telefono" v-model="telefono" />
+            </div>
+
+            <div class="ilDatos">
+              <label class="labelDatos" for="email" >Email:</label>
+              <input class="inputDatos" type="text" id="email" v-model="email" />
+            </div>
+
+          </div>
+        </q-card-section>
+
+        <q-separator />
+
+        <q-card-actions align="right">
+          <q-btn flat label="Cerrar" color="primary" v-close-popup />
+          <q-btn flat label="Aceptar" color="primary" @click="AgregarCliente" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+    <q-btn label="Agregar" color="primary" @click="modal = true" />
+
   </div>
-</div>
 </template>
     
-  <script >
-  import axios from "axios";
-  import { ref } from "vue";
-  
-  const columns = ref([
-    {
-      name: 'Cedula',
-      label: 'Cedula',
-      align: 'left',
-      field: (row) => row.cedula,
-    },
-    {
-      name: 'Nombre',
-      label: 'Nombre',
-      field: (row) => row.nombre,
-    },
-    {
-      name: 'Apellido',
-      label: 'Apellido',
-      field: (row) => row.apellido,
-    },
-    {
-      name: 'Edad',
-      label: 'Edad',
-      field: (row) => row.edad,
-    },
-    {
-      name: 'Telefono',
-      label: 'Telefono',
-      field: (row) => row.telefono,
-    },
-    {
-      name: 'Email',
-      label: 'Email',
-      field: (row) => row.email,
-    },
-    {
-      name: 'Maleta',
-      label: 'Maleta',
-      field: (row) => row.maleta,
-    },
-    {
-      name: "Status",
-      label: "Status",
-      field: (row) => row.status,
-      sortable:true,
-      format:(val)=>(val? 'activo' : 'inactivo')
-    },
-    {
-      name: "Opciones",
-      label: "Opciones",
-      field: "actions",
-    },
-  ])
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
 
-  const rows=ref([]);
-  const id=ref("");
-  const cedula=ref("");
-  const nombre=ref("");
-  const apellido=ref("");
-  const edad=ref("");
-  const telefono=ref("");
-  const email=ref("");
-  const contrasena=ref("");
-  const maleta=ref("");
-  const status = ref(1)
-  const toolbar = ref(false);
+let modal = ref(false)
+let DatosData = ref([]);
+let cedula = ref("");
+let nombre = ref("");
+let telefono = ref("");
+let email = ref("");
+let status = ref("");
 
+const columns = [
+  { name: "cedula", required: true, label: "Cédula", align: "left", field: "cedula", sortable: true },
+  { name: "nombre", required: true, label: "Nombre", align: "left", field: "nombre", sortable: true },
+  { name: "telefono", required: true, label: "Teléfono", align: "left", field: "telefono", sortable: true },
+  { name: "email", required: true, label: "Email", align: "left", field: "email", sortable: true },
+  { name: "status", label: "Status", align: "left", field: "status", sortable: true },
+  { name: "acciones", required: true, label: "Acciones", align: "center", field: "acciones", },
+];
 
 const obtener = async () => {
-try {
-  const response = await axios.get(`https://boleto.onrender.com/api/cliente/ver`);
-  rows.value = response.data;
-  console.log(response);
-} catch (error) {
-  console.error('Error al obtener clientes:', error);
-}
+  try {
+    const response = await axios.get(`https://boleto.onrender.com/api/cliente/ver`);
+    DatosData.value = response.data; 
+  } catch (error) {
+    console.error('Error al obtener clientes:', error);
+  }
 };
 
-const agregarCliente = async () => {
-  const response = await axios.post("https://boleto.onrender.com/api/cliente/agregar", {
+
+async function AgregarCliente() {
+  const data = {
     cedula: cedula.value,
     nombre: nombre.value,
-    apellido: apellido.value,
-    edad: edad.value,
     telefono: telefono.value,
     email: email.value,
-    contrasena: contrasena.value,
-    maleta: maleta.value,
-    status: status.value,
-  });
-  toolbar.value = false;
-  console.log("Cliente agregado:", response.data);
-};
-
-  
-  obtener()
-
-  
-  export default {
-setup() {
-  return {
-    toolbar,
-    selected: ref([]),
-    columns,
-    rows,
-    id,
-    cedula,
-    nombre,
-    apellido,
-    edad,
-    telefono,
-    email,
-    contrasena,
-    maleta,
-    status,
-    agregarCliente
+    status: status.value 
   };
-},
-};
-</script>
+
+  try {
+    let res = await axios.post(`https://boleto.onrender.com/api/cliente/agregar`, data); // Envía los datos del cliente en la solicitud
+    console.log(res);
     
-<style scoped>
-</style>
+
+    cedula.value = "";
+    nombre.value = "";
+    telefono.value = "";
+    email.value = "";
+    status.value = "";
+
+  
+    obtener();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function editarCliente(cliente) {
+  cedula.value = cliente.cedula;
+  nombre.value = cliente.nombre;
+  telefono.value = cliente.telefono;
+  email.value = cliente.email;
+  modal.value = true; 
+}
+
+obtener()
+
+const activar = async (id) => {
+    const cliente = await axios.put(`https://boleto.onrender.com/api/cliente/activar/${id}`);
+    console.log(cliente);
+    if (cliente == null) {
+      return;
+    }
+    const buscar = DatosData.value.findIndex((r) => r._id == id);
+    DatosData.value.splice(buscar, 1, cliente.data.cliente);
+};
+
+const desactivar = async (id) => {
+    const cliente = await axios.put(`https://boleto.onrender.com/api/cliente/inactivar/${id}`);
+    console.log(cliente);
+    if (cliente == null) {
+      return;
+    }
+    const buscar = DatosData.value.findIndex((r) => r._id == id);
+    DatosData.value.splice(buscar, 1, cliente.data.cliente);
+};
+
+
+
+</script>
     
